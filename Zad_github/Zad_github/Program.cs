2f4c53d74abbe4146ac8d1c6a2206d75e72a0462
marti773 <marti_i@abv.bg>
2022-02-14 09:14:52 +0200

    n

---
+++ Zad_github/Zad_github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zad_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
