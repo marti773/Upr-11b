b58000fc9e4411a6550fc47a15a6109f8dbb64da
marti773 <marti_i@abv.bg>
2022-02-15 13:00:50 +0200

    n

---
+++ Zad_spisychen_manipul/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zad_spisychen_manipul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
